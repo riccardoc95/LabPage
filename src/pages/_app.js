3eb7c946885ad2bedfc5d58609a8b9f1855c5c0c
riccardo <riccardo.ceccaroni.95@gmail.com>
2025-09-04 12:54:03 +0200

    updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,9 @@ import { AnimatePresence } from "framer-motion";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Script from "next/script";
+import * as gtag from "@/lib/gtag";
 
 // If loading a variable font, you don't need to specify the font weight
 const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-mont" });
@@ -12,14 +15,41 @@ const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-mont" });
 export default function App({ Component, pageProps }) {
     const router = useRouter();
 
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
+
     return (
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/images/svgs/logo4_.svg" type="image/svg+xml" />
             </Head>
+
+            {/* Google Analytics Scripts */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            />
+            <Script id="gtag-init" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                        page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+
             <main
-                className={`${montserrat.variable} font-mont  bg-light dark:bg-dark w-full min-h-screen h-full`}
+                className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen h-full`}
             >
                 <Navbar />
                 <AnimatePresence initial={false} mode="wait">
